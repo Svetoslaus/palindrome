1c28e69631188d64a8ae025ed001c1f9c7ba9cf0
Svetoslaus <105913393+Svetoslaus@users.noreply.github.com>
2023-02-16 19:39:17 +0100

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+
+
+bool is_palindrome(std::string text){
+ 
+  std::string reverse_text = "";
+
+for (int i = text.size() - 1; i >= 0; i--){
+  reverse_text = reverse_text + text[i];
+}
+
+
+  if (reverse_text == text) {
+     return true;
+  } 
+
+    return false;
+}
+
+int main() {
+  
+  std::cout << is_palindrome("madam") << "\n";
+  std::cout << is_palindrome("ada") << "\n";
+  std::cout << is_palindrome("lovelace") << "\n";
+  
+}
